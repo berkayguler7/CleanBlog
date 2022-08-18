eb22e6fb9924e40b2db24d083c5b82a58fa8c51f
BerkiManman <berkayguler7@hotmail.com>
2022-08-18 14:16:39 +0300

    EJS added

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/index.html'));
+    res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+})
+
+app.get('/post', (req, res) => {
+    res.render('post');
+})
+
+//404
+app.get('*', (req, res) => {
+    res.send('<h1>404 NOT FOUND</h1>');
 })
 
 app.listen(PORT, () => {
